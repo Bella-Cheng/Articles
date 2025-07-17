8291ab1baec6dcdb6157b1251750a3210cd474df
Bella-Cheng <dhp951320@gmail.com>
2025-07-17 17:00:42 +0800

    feat : add footer

---
+++ src/components/home/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-black">
+    <div class="max-w-6xl m-auto py-16">
+      <h3 class="text-3xl text-white font-bold">訂閱電子報<br />
+        <span class="text-sm">VERSE EXPRESS</span>
+      </h3>
+      <p class="text-xs py-10 text-neutral-400">Subscribe VERSE Express<br />
+        to follow the most updated cultural views.
+      </p>
+      <div class="border rounded-full border-white w-100 flex justify-between items-center px-8 focus-within:border-[#ffa500]">
+        <input class="w-full placeholder-neutral-400 text-left py-2 mr-4 focus:outline-none text-white" type="email" name="user-email" placeholder="ENTER YOUR EMAIL">
+        <button class="cursor-pointer" type="submit"><i class="fa-solid fa-angle-right text-white"></i></button>
+      </div>
+      <div class="text-neutral-400 text-center leading-4 text-xs pt-20">
+        <p><a class="cursor-pointer hover:text-white" href="http://">關於我們</a> | <a class="cursor-pointer hover:text-white" href="http://">隱私政策</a></p><br />
+        <p>106 台北市大安區建國南路一段 177 號 | (02) 2550 0065 | <a class="cursor-pointer hover:text-white" href="mailto:">dhp951320@gmail.com</a> | © VERSE All rights reserved. | vm2</p>
+      </div>
+
+
+
+
+    </div>
+
+  </div>
+</template>
