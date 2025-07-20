adf92b035ec3da5ce626d5e1f64586d76eda7586
Bella-Cheng <dhp951320@gmail.com>
2025-07-20 12:34:46 +0800

    feat : shopping cart

--- src/components/home/Nav.vue
+++ src/components/home/Nav.vue
@@ -3,7 +3,7 @@
   import { articles, voice, magazine } from '@/data/data'
   import { ref, computed } from 'vue'
   import dayjs from 'dayjs'
-
+  
   const menuTab = ref('')
   const toggleMenu = ref(false)
 
@@ -54,46 +54,46 @@
       <div class="flex">
         <ul class="flex py-2">
           <router-link to="/articles" class="cursor-pointer">
-            人物
+            <span class="hover:text-[#ffa500]">人物</span>
           </router-link>
 
           <router-link to="/articles/figure" 
             @click="handdleTab('articles')" 
             class="before:mx-2 before:font-bold before:content-['‧'] cursor-pointer"
           >
-            文章
+            <span class="hover:text-[#ffa500]">文章</span>
           </router-link> 
 
           <router-link to="/features" 
             class="before:mx-2 before:font-bold before:content-['‧'] cursor-pointer"
           >
-            專題
+            <span class="hover:text-[#ffa500]">專題</span>
           </router-link>
 
           <router-link to="/podcasts" 
             @click="handdleTab('voice')" 
             class="before:mx-2 before:font-bold before:content-['‧'] cursor-pointer"
           >
-            聲音
+            <span class="hover:text-[#ffa500]">聲音</span>
           </router-link>
 
           <router-link to="/magazines" 
             @click="handdleTab('magazine')" 
             class="before:mx-2 before:font-bold before:content-['‧'] cursor-pointer"
           >
-            雜誌
+            <span class="hover:text-[#ffa500]">雜誌</span>
           </router-link>
 
           <router-link to="/articles/tag/vibes" 
             class="before:mx-2 before:font-bold before:content-['‧'] cursor-pointer"
           >
-            VIBES
+            <span class="hover:text-[#ffa500]">VIBES</span>
           </router-link>
 
           <router-link to="/shop/cool-stuff" 
             class="before:mx-2 before:font-bold before:content-['‧'] cursor-pointer"
           >
-            V Shop
+            <span class="hover:text-[#ffa500]">V Shop</span>
           </router-link>
 
           <router-link to="/shop/subscription/one-year-plan" 
@@ -105,26 +105,33 @@
 
       </div>
       <div class="flex py-2 items-center cursor-pointer">
-        <i class="fa-solid fa-moon mx-2 cursor-pointer"></i>
+        <i class="fa-solid fa-moon mx-2 cursor-pointer hover:text-[#ffa500]"></i>
         
-        <i class="fa-solid fa-magnifying-glass mx-2 cursor-pointer"></i>
+        <i class="fa-solid fa-magnifying-glass mx-2 cursor-pointer hover:text-[#ffa500]"></i>
         
         <router-link to="/login">
-          <i class="fa-regular fa-circle-user mx-2 cursor-pointer"></i>
+          <i class="fa-regular fa-circle-user mx-2 cursor-pointer hover:text-[#ffa500]"></i>
         </router-link>
 
         <router-link to="/bag">
-          <i class="fa-solid fa-bag-shopping mx-2 cursor-pointer"></i>
+          <i class="fa-solid fa-bag-shopping mx-2 cursor-pointer hover:text-[#ffa500]"></i>
         </router-link>
         
-        <a href="mailto:dhp951320@gmail.com"><i class="fa-solid fa-envelope mx-2 cursor-pointer"></i></a>
+        <a href="mailto:dhp951320@gmail.com"><i class="fa-solid fa-envelope mx-2 cursor-pointer hover:text-[#ffa500]"></i></a>
         
         <router-link to="/member">
-          <img 
-            class="w-5 h-5 ml-2 cursor-pointer"
-            src="@/assets/GitHub.png"
-            alt="GitHub"
+          <svg
+            class="w-5 h-5 ml-2 cursor-pointer hover:fill-[#ffa500] transition"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 97.7 97.7"
           >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+            />
+          </svg>
         </router-link>
       </div>
     </div>
--- src/views/bag/ShopCart.vue
+++ src/views/bag/ShopCart.vue
@@ -1,27 +1,73 @@
 <template>
-  <div class="max-w-6xl m-auto">
-    <div>
-      <div>
-        <img src="" alt="">
-      </div>
-      <div>
-        <span>NT$</span>
-        <span>X</span>
-        <div>
-          <input type="number">
-          <div>
-            <button type="button"><i class="fa-solid fa-caret-up"></i></button>
-            <button type="button"><i class="fa-solid fa-caret-down"></i></button>
+  <div class="bg-gray-50 min-h-screen py-12 px-4">
+    <div class="text-3xl font-bold py-8 mb-6 text-center">購物袋</div>
+    <div class="max-w-6xl mx-auto flex flex-col lg:flex-row gap-10">
+
+      <!-- 購物車列表 -->
+      <div class="flex-1">
+        <div class="divide-y">
+          <!-- 商品 -->
+          <div class="flex flex-col sm:flex-row items-start py-8 gap-6">
+            <img src="@/assets/bar.jpg" alt="Nomad Tumbler" class="w-32 h-32 object-cover rounded-xl" />
+            <div class="flex-1 w-full">
+              <div class="flex justify-between items-start">
+                <div>
+                  <a href="http://">
+                    <div class="font-semibold text-lg hover:text-gray-600">愛上花蓮的20個理由
+                      <span class="flex gap-4 text-gray-700 text-sm mt-1">米老鼠封面</span>
+                    </div>
+                  </a>
+                  <div class="mt-2 font-bold text-gray-900">NT$ 350</div>
+                </div>
+
+                <div class="flex items-center border-gray-100">
+                  <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-black hover:text-white"> - </span>
+                  <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="text" value="1" min="1" />
+                  <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-black hover:text-white"> + </span>
+                </div> 
+
+                <button class="text-2xl text-gray-600 hover:text-red-800 ml-3">&times;</button>
+              </div>
+
+            </div>
           </div>
 
         </div>
-        <span>NT$</span>
-
-
       </div>
 
+      <!-- 訂單摘要 -->
+      <div class="w-full lg:w-96 flex-shrink-0">
+        <div class="bg-white p-8">
+          <h3 class="text-xl font-bold mb-6">訂單明細</h3>
+          <ul class="text-gray-700 mb-6">
+            <li class="flex justify-between items-center mb-3">
+              <span>商品數量</span>
+              <span class="font-semibold">1 件</span>
+            </li>
+            <li class="flex justify-between items-center mb-3">
+              <span>商品金額</span>
+              <span class="font-semibold">NT$ 350</span>
+            </li>
+            <li class="flex justify-between items-center mb-3">
+              <span class="flex items-center">運費
+                <span class="inline-block align-middle text-gray-400 pl-2 pb-1" title="滿 3000 元可免運">
+                  <svg class="w-4 h-4 inline hover:text-[#ffa500] transition" fill="none" stroke="currentColor" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10" stroke-width="2" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 16v-4m0-4h.01" />
+                  </svg>
+                </span>
+              </span>
 
+              
+              <span class="font-semibold">NT$ 100</span>
+            </li>
+
+            <li class="flex justify-between items-center border-t pt-4 mt-4 text-lg">
+              <span>總金額</span>
+              <span class="font-bold">NT$ 450</span>
+            </li>
+          </ul>
+          <button class="w-full py-3 rounded-xl bg-black text-white font-semibold hover:bg-[#ffa500] transition text-lg">結帳</button>
+        </div>
+      </div>
     </div>
   </div>
-
 </template>
